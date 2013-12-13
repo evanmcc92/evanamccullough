e28767f0b853bb1d07d661f278686871811e2ec8
Evan McCullough <webmaster@evanamccullough.com>
2013-12-12 21:25:23 -0500

    updates to project and index

--- scripts/googleAnalytics.js
+++ scripts/googleAnalytics.js
@@ -1,3 +1,4 @@
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -5,3 +6,7 @@
 
   ga('create', 'UA-42109105-1', 'evanamccullough.com');
   ga('send', 'pageview');
+  ga('send', 'event', {'eventCategory': '_trackEvent', 'eventAction': 'Download', 'eventLabel': 'Resume'});
+  ga('send', 'event', {'eventCategory': '_trackEvent', 'eventAction': 'button', 'eventLabel': 'who'});
+  ga('send', 'event', {'eventCategory': '_trackEvent', 'eventAction': 'button', 'eventLabel': 'what'});
+  ga('send', 'event', {'eventCategory': '_trackEvent', 'eventAction': 'button', 'eventLabel': 'why'});
